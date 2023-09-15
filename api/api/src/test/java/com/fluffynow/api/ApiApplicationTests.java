3548142f7ce5e01711d98d59eb2cfcd6e2b1a850
Victor <victorh_delete@hotmail.com>
2023-09-14 23:42:42 -0300

    First Commit
    Inicialização da API com SpringBoot Inicializer e inicio da criação de UML de representação de dados do terminal.

---
+++ api/api/src/test/java/com/fluffynow/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffynow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
